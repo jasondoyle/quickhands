2050e7ae2ffd2f31468d6934f7cadc07d3e2361f
Jason Doyle <doylej.ia@gmail.com>
2022-03-26 22:17:59 -0400

    hopefully better async

--- backend_scripts/Mempool.js
+++ backend_scripts/Mempool.js
@@ -6,8 +6,6 @@ const User = require('./User.js');
 const { masterKey, appId, serverUrl } = process.env;
 const { QUICKHANDS_CONTRACT_ADDRESS, HTTPS_PROVIDER_BSCMAINNET, WSS_PROVIDER_BSCMAINNET } = process.env;
 
-//class QHEmitter extends EventEmitter { };
-
 class Mempool extends EventEmitter {
     web3;
     web3ws;
@@ -25,13 +23,15 @@ class Mempool extends EventEmitter {
     }
 
     watchTransactions = () => {
-        // only watching for scams using methods
+        let txHashList = [];
+
+        // only watching for scams using these methods
         const approveMethodId = '0x095ea7b3';
         const transferMethodId = '0xa9059cbb';
 
         console.log('[+] Monitoring Active');
         this.users.forEach( (user) => {
-            console.log('[+] User => ' + user.userAddr + '\n');
+            console.log('[+] User => ' + user.userAddr);
         });
 
         let subscription = this.web3ws.eth.subscribe('pendingTransactions', (err, res) => {
@@ -40,38 +40,51 @@ class Mempool extends EventEmitter {
             }
         });
 
+        const scanTx = (tx) => {
+            if (!tx || !tx.to) return;
+
+            let from = tx.from.toLowerCase();
+            let token = tx.to.toLowerCase();
+            let value = this.web3.utils.fromWei(tx.value, 'ether');
+            let method = tx.input.slice(0, 10);
+            console.log('tx hash ====> ' , tx.hash, '[', method, ']' );
+
+            this.users.forEach( (user) => {
+                // check if tx targets our user and one of their enrolled tokens
+                if (from != user.userAddr) return
+                if (!user.enrolledTokens.includes(token) || user.triggered.includes(token)) return
+                if (method != approveMethodId && method != transferMethodId) return
+
+                this.emit('scam', {
+                    'user': user, // QH User object
+                    'hash': tx.hash,
+                    'victim': from,
+                    'token': token,
+                    'gasPrice': tx.gasPrice,
+                    'gas': tx.gas
+                });
+                subscription.unsubscribe();  // hack for demo since throttled
+            });
+            return;
+        }
+
+        const fetchTx = (txHash) => {
+            return this.web3.eth.getTransaction(txHash);
+        }
+
         subscription.on('data', (txHash) => {
-            setTimeout( async () => {
-                try {                   
-                    let tx = await this.web3.eth.getTransaction(txHash);
+            txHashList.push(txHash);
 
-                    if (!tx || !tx.to) return;
-
-                    let from = tx.from.toLowerCase();
-                    let token = tx.to.toLowerCase();
-                    let value = this.web3.utils.fromWei(tx.value, 'ether');
-                    let method = tx.input.slice(0, 10);
-                    console.log('tx hash ====> ' , tx.hash, '[', method, ']' );
-
-                    this.users.forEach( (user) => {
-                        // check if tx targets our user and one of their enrolled tokens
-                        if (from != user.userAddr) return
-                        if (!user.enrolledTokens.includes(token) || user.triggered.includes(token)) return
-                        if (method != approveMethodId && method != transferMethodId) return
-
-                        this.emit('scam', {
-                            'user': user, // QH User object
-                            'hash': tx.hash,
-                            'victim': from,
-                            'token': token,
-                            'gasPrice': tx.gasPrice,
-                            'gas': tx.gas
-                        });   
-                        subscription.unsubscribe();  // hack for demo since throttled
-                    })
-                } catch (err) {
+            setTimeout( async function() {
+                try {
+                    const txPromises = txHashList.map(hash => fetchTx(hash));
+                    const txResults = await Promise.all(txPromises);
+                    const scanTxPromises = txResults.map(tx => scanTx(tx));
+                    await Promise.all(scanTxPromises);
+                } catch(err) {
                     console.error(err);
                 }
+                txHashList = []; 
             }, 500) // 1000 = 1 second
         });
     }
